e76009d191110ccc642ad1fe33c9289f8f2f9939
SarthakJShetty <sarthakshetty97@gmail.com>
2019-04-29 19:13:46 +0530

    Adding the build and devel files here as well. Will be migrating to the server side of things soon.

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/sarthak/projects/Transient/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/sarthak/projects/Transient/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/sarthak/projects/Transient/catkin_ws/devel;/opt/ros/kinetic" != "" else []
---
+++ catkin_ws/build/transient/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sarthak/projects/Transient/catkin_ws/src/transient
+# Build directory: /home/sarthak/projects/Transient/catkin_ws/build/transient
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
