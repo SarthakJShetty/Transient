cf24247f9ebc5824abb9939de680c9be97085f64
SarthakJShetty <sarthakshetty97@gmail.com>
2019-05-24 15:26:27 +0530

    Got rid of the init_node command post integration with navigation stack.

--- catkin_ws/src/transient/scripts/scan.py
+++ catkin_ws/src/transient/scripts/scan.py
@@ -5,12 +5,7 @@ from goforward_and_avoid_obstacle import GoForwardAvoid
 '''def callback(message):
 	return str(message.ranges[360])'''
 
-def main_function():
-	rospy.init_node('laser_scanner', anonymous=True)
+def sensor_value():
+	'''rospy.init_node('laser_scanner', anonymous=True)'''
 	sub = rospy.wait_for_message('/scan', LaserScan)
 	return str(sub.ranges[360])
-
-lidar_reading = main_function()
-
-if (lidar_reading<'5'):
-	GoForwardAvoid()
