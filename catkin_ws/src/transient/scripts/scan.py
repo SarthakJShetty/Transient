9b6b8b2c8be2837af5226f5214f258af8da8b405
SarthakJShetty <sarthakshetty97@gmail.com>
2019-05-27 16:48:32 +0530

    Reintroduced the init_node() function. Will evoke if script has to be singularly evoked.

--- catkin_ws/src/transient/scripts/scan.py
+++ catkin_ws/src/transient/scripts/scan.py
@@ -2,10 +2,9 @@ import rospy
 from sensor_msgs.msg import LaserScan
 from goforward_and_avoid_obstacle import GoForwardAvoid
 
-'''def callback(message):
-	return str(message.ranges[360])'''
+def scan_init_node():
+	rospy.init_node('laser_scanner', anonymous=True)
 
 def sensor_value():
-	'''rospy.init_node('laser_scanner', anonymous=True)'''
 	sub = rospy.wait_for_message('/scan', LaserScan)
 	return str(sub.ranges[360])
